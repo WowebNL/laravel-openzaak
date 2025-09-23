d1e860ed99330a193424cc9998ad209d0e044bf7
Michel <79970817+Michel-Verhoeven@users.noreply.github.com>
2025-09-23 12:51:36 +0200

    Add Patch and Put actions to Enkelvoudiginformatieobjecten

--- src/Api/Endpoints/Documenten/Enkelvoudiginformatieobjecten.php
+++ src/Api/Endpoints/Documenten/Enkelvoudiginformatieobjecten.php
@@ -6,10 +6,12 @@ use Woweb\Openzaak\Api\Endpoints\AbstractEndpoint;
 use Woweb\Openzaak\Api\Actions\getAll;
 use Woweb\Openzaak\Api\Actions\getSingle;
 use Woweb\Openzaak\Api\Actions\Store;
+use Woweb\Openzaak\Api\Actions\Patch;
+use Woweb\Openzaak\Api\Actions\Put;
 
 class Enkelvoudiginformatieobjecten extends AbstractEndpoint
 {
-    use GetAll, GetSingle, Store;
+    use GetAll, GetSingle, Store, Patch, Put;
 
     protected $apiName = 'documenten';
 
