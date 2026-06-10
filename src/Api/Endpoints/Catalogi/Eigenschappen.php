e17356a1d72b79eb9d7011877fa3c294c8d838fd
Michel <79970817+Michel-Verhoeven@users.noreply.github.com>
2026-06-10 13:55:39 +0200

    Add Store action to Eigenschappen endpoint

--- src/Api/Endpoints/Catalogi/Eigenschappen.php
+++ src/Api/Endpoints/Catalogi/Eigenschappen.php
@@ -4,11 +4,12 @@ namespace Woweb\Openzaak\Api\Endpoints\Catalogi;
 
 use Woweb\Openzaak\Api\Actions\getAll;
 use Woweb\Openzaak\Api\Actions\getSingle;
+use Woweb\Openzaak\Api\Actions\Store;
 use Woweb\Openzaak\Api\Endpoints\AbstractEndpoint;
 
 class Eigenschappen extends AbstractEndpoint
 {
-    use GetAll, GetSingle;
+    use GetAll, GetSingle, Store;
 
     protected $apiName = 'catalogi';
 
